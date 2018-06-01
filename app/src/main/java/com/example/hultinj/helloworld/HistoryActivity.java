52b7eebd0a52f6249f730bbaf2a4ddef550c5ae7
Jie Tao <taoj@mail.gvsu.edu>
2018-05-31 22:41:21 -0400

    v1.0

---
+++ app/src/main/java/com/example/hultinj/helloworld/HistoryActivity.java
@@ -0,0 +1,44 @@
+package com.example.hultinj.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.hultinj.helloworld.dummy.HistoryContent;
+import com.example.hultinj.helloworld.dummy.HistoryContent.HistoryItem;
+
+public class HistoryActivity extends AppCompatActivity
+        implements HistoryFragment.OnListFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onListFragmentInteraction(HistoryItem item) {
+        System.out.println("Interact!");
+        Intent intent = new Intent();
+        String[] vals = {item.origLat, item.origLng, item.destLat, item.destLng};
+        intent.putExtra("item", vals);
+        setResult(MainActivity.HISTORY_RESULT,intent);
+        finish();
+    }
+
+}
--- app/src/main/java/com/example/hultinj/helloworld/MainActivity.java
+++ app/src/main/java/com/example/hultinj/helloworld/MainActivity.java
@@ -6,7 +6,7 @@
 
 package com.example.hultinj.helloworld;
 
-        import android.content.Context;
+import android.content.Context;
         import android.content.Intent;
         import android.location.Location;
         import android.support.v7.app.AppCompatActivity;
@@ -20,7 +20,11 @@ package com.example.hultinj.helloworld;
         import android.widget.TextView;
         import android.widget.Toast;
 
-        import java.nio.channels.FileLock;
+import com.example.hultinj.helloworld.dummy.HistoryContent;
+
+import org.joda.time.DateTime;
+
+import java.nio.channels.FileLock;
         import java.text.ParseException;
         import java.time.Duration;
 
@@ -28,9 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
     private float distanceInKilometers;
     private double bearingInDegrees;
-    private static final int FEEDBACK = 1;
+    public static final int FEEDBACK = 100;
+    public static final int HISTORY_RESULT = 101;
     private static String distanceUnit="Kilometers";
     private static String bearingUnit="Degrees";
+
+    Button calculateBtn;
+    Button clearBtn;
+
+
+    EditText longitude1EditText;
+    EditText longitude2EditText;
+    EditText latitude1EditText;
+    EditText latitude2EditText;
+
+    TextView distanceLabel;
+    TextView bearingLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,74 +56,20 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(findViewById(R.id.my_toolbar));
 
-        Button calculateBtn = findViewById(R.id.calculateBtn);
-        Button clearBtn = findViewById(R.id.clearBtn);
+        calculateBtn = findViewById(R.id.calculateBtn);
+        clearBtn = findViewById(R.id.clearBtn);
+        longitude1EditText = findViewById(R.id.longitude1);
+        longitude2EditText = findViewById(R.id.longitude2);
+        latitude1EditText = findViewById(R.id.latitude1);
+        latitude2EditText = findViewById(R.id.latitude2);
 
-
-        EditText longitude1EditText = findViewById(R.id.longitude1);
-        EditText longitude2EditText = findViewById(R.id.longitude2);
-        EditText latitude1EditText = findViewById(R.id.latitude1);
-        EditText latitude2EditText = findViewById(R.id.latitude2);
-
-        TextView distanceLabel = findViewById(R.id.distanceLbl);
-        TextView bearingLabel = findViewById(R.id.bearingLbl);
+         distanceLabel = findViewById(R.id.distanceLbl);
+         bearingLabel = findViewById(R.id.bearingLbl);
 
         calculateBtn.setOnClickListener(v -> {
-
-            View view = this.getCurrentFocus();
-            if (view != null) {
-                InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-            }
-
-            String lat1 = latitude1EditText.getText().toString();
-            String lon1 = longitude1EditText.getText().toString();
-            String lat2 = latitude2EditText.getText().toString();
-            String lon2 = longitude2EditText.getText().toString();
-
-            if( lat1 == null || lat1.isEmpty() ||
-                lon1 == null || lon1.isEmpty() ||
-                lat2 == null || lat2.isEmpty() ||
-                lon2 == null || lon2.isEmpty()) {
-
-                Toast.makeText(this, R.string.enterValidValues, Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            Double lat1D;
-            Double lon1D;
-            Double lat2D;
-            Double lon2D;
-
-            try {
-               lat1D = (Double.parseDouble(lat1));
-               lon1D = (Double.parseDouble(lon1));
-               lat2D = (Double.parseDouble(lat2));
-               lon2D = (Double.parseDouble(lon2));
-            } catch (Exception parseException) {
-                Toast.makeText(this, R.string.enterValidValues, Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            Location loc1 = new Location("");
-            loc1.setLatitude(lat1D);
-            loc1.setLongitude(lon1D);
-
-            Location loc2 = new Location("");
-            loc2.setLatitude(lat2D);
-            loc2.setLongitude(lon2D);
-
-            this.distanceInKilometers = loc1.distanceTo(loc2) / 1000.0f;
-            this.bearingInDegrees = loc1.bearingTo(loc2);
-            
-            //double dLon = (lon2D-lon1D);
-            //double y = Math.sin(dLon) * Math.cos(lat2D);
-            //double x = Math.cos(lat1D)*Math.sin(lat2D) - Math.sin(lat1D)*Math.cos(lat2D)*Math.cos(dLon);
-            //this.bearingInDegrees = Math.toDegrees((Math.atan2(y, x)));
-            //this.bearingInDegrees =  ((brng + 360) % 360);
-
-            redrawLabels();
+            updateScreen();
         });
+
 
         clearBtn.setOnClickListener(v -> {
 
@@ -167,12 +130,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
         switch (item.getItemId()) {
+
             case R.id.action_setting:
-
-                Intent intent = new Intent(this, SettingsActivity.class);
+                intent = new Intent(this, SettingsActivity.class);
                 startActivityForResult(intent,FEEDBACK);
-
+                return true;
+            case R.id.action_history:
+                intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivityForResult(intent, HISTORY_RESULT );
                 return true;
             default:
                 // If we got here, the user's action was not recognized.
@@ -191,6 +158,80 @@ public class MainActivity extends AppCompatActivity {
                 bearingUnit = data.getStringExtra("bearingUnit");
                 redrawLabels();
             }
+
         }
+        else if(requestCode==HISTORY_RESULT){
+
+            String[] vals = data.getStringArrayExtra("item");
+            this.latitude1EditText.setText(vals[0]);
+            this.longitude1EditText.setText(vals[1]);
+            this.latitude2EditText.setText(vals[2]);
+            this.longitude2EditText.setText(vals[3]);
+            this.updateScreen();
+        }
+    }
+
+    //recalculate the data and refresh screen
+    private void updateScreen(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        String lat1 = latitude1EditText.getText().toString();
+        String lon1 = longitude1EditText.getText().toString();
+        String lat2 = latitude2EditText.getText().toString();
+        String lon2 = longitude2EditText.getText().toString();
+
+        if( lat1 == null || lat1.isEmpty() ||
+                lon1 == null || lon1.isEmpty() ||
+                lat2 == null || lat2.isEmpty() ||
+                lon2 == null || lon2.isEmpty()) {
+
+            Toast.makeText(this, R.string.enterValidValues, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Double lat1D;
+        Double lon1D;
+        Double lat2D;
+        Double lon2D;
+
+        try {
+            lat1D = (Double.parseDouble(lat1));
+            lon1D = (Double.parseDouble(lon1));
+            lat2D = (Double.parseDouble(lat2));
+            lon2D = (Double.parseDouble(lon2));
+        } catch (Exception parseException) {
+            Toast.makeText(this, R.string.enterValidValues, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Location loc1 = new Location("");
+        loc1.setLatitude(lat1D);
+        loc1.setLongitude(lon1D);
+
+        Location loc2 = new Location("");
+        loc2.setLatitude(lat2D);
+        loc2.setLongitude(lon2D);
+
+        this.distanceInKilometers = loc1.distanceTo(loc2) / 1000.0f;
+        this.bearingInDegrees = loc1.bearingTo(loc2);
+
+        //double dLon = (lon2D-lon1D);
+        //double y = Math.sin(dLon) * Math.cos(lat2D);
+        //double x = Math.cos(lat1D)*Math.sin(lat2D) - Math.sin(lat1D)*Math.cos(lat2D)*Math.cos(dLon);
+        //this.bearingInDegrees = Math.toDegrees((Math.atan2(y, x)));
+        //this.bearingInDegrees =  ((brng + 360) % 360);
+
+        redrawLabels();
+
+        //presist history
+        HistoryContent.HistoryItem item = new
+                HistoryContent.HistoryItem(this.latitude1EditText.getText().toString(),
+                this.longitude1EditText.getText().toString(), this.latitude2EditText.getText().toString(),
+                this.longitude2EditText.getText().toString(), DateTime.now());
+        HistoryContent.addItem(item);
     }
 }
